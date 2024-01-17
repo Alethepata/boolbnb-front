586e623d7af9d56c95414bf5ca3ca2fdd4f12f9e
Alethepata <alessia.patane@gmail.com>
2024-01-17 19:50:11 +0100

    [add] card

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,41 @@
 <script>
+import axios from 'axios';
+import { store } from '../data/store';
+import ApartmentCards from '../components/partials/ApartmentCards.vue';
 export default {
-    name:'Home',
+  name: 'Home',
+  components: {
+    ApartmentCards
+  },
+  data() {
+    return {
+      store
+      }
+  },
+  methods: {
+    getApi(endpoint) {
+      axios.get(endpoint)
+        .then(res => {
+          store.apartments = res.data.apartments;
+          // console.log(store.apartments)
+      })
+    }
+  },
+  mounted() {
+    this.getApi(store.apiUrl + 'apartments')
+
+
+  }
+
 }
+
 </script>
 
 <template>
   <h1>Home</h1>
+   <div class="container d-flex flex-wrap justify-content-evenly">
+    <ApartmentCards/>
+   </div>
 </template>
 
 <style>
