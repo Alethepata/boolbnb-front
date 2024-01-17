5224e4d9d47f44a17fb35074fcaf9c0dd2d3c0b5
Alethepata <alessia.patane@gmail.com>
2024-01-17 18:38:42 +0100

    [add] store

--- src/pages/RicercaAppartamenti.vue
+++ src/pages/RicercaAppartamenti.vue
@@ -1,11 +1,33 @@
 <script>
+import axios from 'axios';
+import { store } from '../data/store';
+
 export default {
-    name:'RicercaAppartamenti',
+  name: 'RicercaAppartamenti',
+  components: {
+      
+  },
+  data() {
+    return {
+      store,
+        search:''
+      }
+  },
+  methods: {
+    getApi() {
+      axios.get(store.apiUrl + 'apartments')
+    }
+  }
 }
 </script>
 
 <template>
   <h1>Ricerca</h1>
+  <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" v-model="search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+
 </template>
 
 <style>
