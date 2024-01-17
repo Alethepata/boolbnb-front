8ed8baa40f91888f5e483f3ce9cf3af39e24b8a0
manueledigregorio <dig.manuele@gmail.com>
2024-01-17 19:03:24 +0100

    [add] card apartments

---
+++ src/components/partials/ApartmentCards.vue
@@ -0,0 +1,27 @@
+
+<script>
+import { store } from "../data/store";
+export default {
+  name: "ApartmentCards",
+  data() {
+    return {
+      store,
+    };
+  },
+};
+</script>
+
+<template>
+  <div v-for="apartment in store.apartments" :key="apartment.id" class="card" style="width: 18rem">
+    <img src="{{ apartment.image }}" class="card-img-top" alt="..." />
+    <div class="card-body">
+      <div class="card-text">
+        <h2>{{ apartment.title }}</h2>
+        <p>{{ apartment.address }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
