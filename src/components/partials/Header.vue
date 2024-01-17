6acb6718d66c644ebc1a79d5b31dd84bd43cd9c1
Alethepata <alessia.patane@gmail.com>
2024-01-17 17:09:05 +0100

    [init]

---
+++ src/components/partials/Header.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    name:'Header'
+}
+</script>
+
+<template>
+    <header>
+        <div class="container d-flex justify-content-between my-3">
+                <div class="logo">
+                  <p>
+                    logo
+                  </p>
+                </div>
+                <div class="navbar">
+                    <nav>
+                        <ul class="d-flex">
+                            <li>
+                                <a href="#">Cerca appartamento</a>
+                            </li>
+                            <li class="mx-4">
+                                <a href="#">Sponsor</a>
+                            </li>
+                            <li>
+                                <a href="#">Registrati</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+
+        </div>
+    </header>
+</template>
+
+<style>
+
+</style>
