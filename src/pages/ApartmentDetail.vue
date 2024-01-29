5ae859dfc58a1e9dcc16961ec3da7630ada14489
Idedav <xdavide.correa@gmail.com>
2024-01-29 11:45:35 +0100

    [add] map on detail apartment

--- src/pages/ApartmentDetail.vue
+++ src/pages/ApartmentDetail.vue
@@ -21,6 +21,7 @@ export default {
         .get(store.apiUrl + "apartments/apartment-detail/" + slug)
         .then((res) => {
           this.apartment = res.data.apartment;
+          this.initMap();
         });
     },
 
@@ -42,14 +43,39 @@ export default {
       axios.get("https://api.ipify.org?format=json")
       .then(res=>{
         this.ip_address = res.data.ip;
-        console.log(this.ip_address);
         axios.get(store.apiUrl + "save-view/" + this.apartment.id + "/" + this.ip_address)
         .then(res=>{
           console.log(res.data);
         })
       })
 
-    }
+    },
+    initMap() {
+
+          let map;
+
+          map = tt.map({
+            key: store.tomTomApiKey,
+            container: 'map',
+            basePath: 'sdk/',
+            center: [this.apartment.longitude, this.apartment.latitude],
+            zoom: 16,
+            theme: {
+              style: 'buildings',
+              layer: 'basic',
+              source: 'vector'
+            },
+          });
+          map.addControl(new tt.NavigationControl());
+
+          const element = document.getElementById('marker');
+          const marker = new tt.Marker({
+            element: element,
+            anchor: 'center',
+          }).setLngLat([this.apartment.longitude, this.apartment.latitude]);
+
+          marker.addTo(map);
+    },
   },
   computed: {
     servicesList() {
@@ -66,22 +92,27 @@ export default {
 </script>
 
 <template>
-  <div class="container my-5">
-    <h1 class="my-4">{{ apartment.title }}</h1>
+  <div class="container">
+    <h2 class="my-4">{{ apartment.title }}</h2>
     <p>{{ apartment.address }}</p>
     <div class="d-flex">
       <div class="image me-5">
         <img :src="apartment.img" alt="" />
       </div>
       <div>
-        <h3>Dettagli:</h3>
-        <ul>
-          <li>Camere totali: {{ apartment.rooms }}</li>
-          <li>Bagni totali: {{ apartment.bathrooms }}</li>
-          <li>Posti letto totali: {{ apartment.beds }}</li>
-        </ul>
-        <h3>Servizi aggiuntivi:</h3>
-        <p>{{ servicesList }}</p>
+        <div>
+          <h5>Dettagli:</h5>
+          <ul>
+            <li>Camere totali: {{ apartment.rooms }}</li>
+            <li>Bagni totali: {{ apartment.bathrooms }}</li>
+            <li>Posti letto totali: {{ apartment.beds }}</li>
+          </ul>
+        </div>
+        <div>
+          <h5>Servizi aggiuntivi:</h5>
+          <p>{{ servicesList }}</p>
+        </div>
+        <div id="map" class="map"></div>
       </div>
     </div>
     <div>
@@ -120,17 +151,25 @@ export default {
 
     </div>
   </div>
+
 </template>
 
 <style lang="scss" scoped>
+
+
+.container{
+  padding-top: 100px;
+}
 .image {
   width: 800px;
+  border-radius: 10px;
   img {
     width: 100%;
+    border-radius: 10px;
   }
 }
-h3,
-h1 {
+h5,
+h2 {
   color: black;
 }
 
@@ -139,5 +178,10 @@ ul {
   li {
     color: black;
   }
+}
+
+.map{
+  width: 500px;
+  height: 300px;
 }
 </style>
